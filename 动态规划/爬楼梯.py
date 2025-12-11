5236d6bacff19155ca72b5c78c7f1dc72f9655aa
十二 <chengweiluo12@gmail.com>
2025-12-11 10:07:52 +0800

    2025.12.11

---
+++ 动态规划/爬楼梯.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        f0=f1=1
+        for _ in range(2,n+1):
+            new_f1=f0+f1
+            f0=f1
+            f1=new_f1
+        return f1
