25b3e0ab4e0a03890112abc6df0274238b46fdc4
十二 <chengweiluo12@gmail.com>
2025-12-27 16:30:38 +0800

    2025.12.27

---
+++ 技巧/多元素数.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n=len(nums)
+        cnt=Counter(nums)
+        for num,value in cnt.items():
+            if value>(n//2):
+                return num
+    def majorityElement(self, nums: List[int]) -> int:
+        ans = hp = 0
+        for x in nums:
+            if hp == 0:  # x 是初始擂主，生命值为 1
+                ans, hp = x, 1
+            else:  # 比武，同门加血，否则扣血
+                hp += 1 if x == ans else -1
+        return ans
