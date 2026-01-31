5d4f75bc8472274f04a48e32a6f8c0530a39b812
十二 <chengweiluo12@gmail.com>
2026-01-31 15:00:29 +0800

    2026.01.31

---
+++ 经典150/栈/逆波兰表达式求值.py
@@ -0,0 +1,19 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack=[]
+        # res=0
+        for c in tokens:
+            if c not in "+-*/":
+                stack.append(int(c))
+            else:
+                a=stack.pop()
+                b=stack.pop()
+                if c=="+":
+                    stack.append(b+a)
+                elif c=="-":
+                    stack.append(b-a)
+                elif c=="*":
+                    stack.append(b*a)
+                elif c=="/":
+                    stack.append(int(b/a))
+        return stack[-1]
