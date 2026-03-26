1c51ac986ee505cf84cbc8b7f776222ab9b6ea92
十二 <chengweiluo12@gmail.com>
2026-03-26 18:36:31 +0800

    2026.03.26

---
+++ Top100-2.0/二叉树/二叉树的中序遍历.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res=[]
+        def dfs(root):
+            if root is None:
+                return None
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        return res
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        stack=[]
+        res=[]
+        cur=root
+        while cur or stack:
+            if cur:
+                stack.append(cur)
+                cur=cur.left
+            else:
+                cur=stack.pop()
+                res.append(cur.val)
+                cur=cur.right
+        return res
